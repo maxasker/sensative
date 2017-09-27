60a8a74ff2c1f1627227d28902c3e66c61cbf225
Max Renvalder Asker <maxasker@hotmail.com>
2017-09-27 13:05:14 +0200

    instruktioner

---
+++ nodeTest/expresstest/instruktioner.js
@@ -0,0 +1,16 @@
+/*
+INSTRUKTIONER FÖR ATT FÅ IGÅNG NODE/EXPRESS, ska du köra mitt så strunta i rad 8
+
+installera node via npm (kmr inte ihåg kommando, antar npm install node(.js?)), får ladda ner det via deras hemsida också
+Gör din mapp
+cd in i mappen
+npm install -g expess-generator                     express är ett ramverk för node(med routes/views etc)
+express {PROJEKTNAMN}
+cd in i projektet
+npm install
+npm start
+localhost:3000
+kolla sedan app.js och leta kommentarer på svenska.
+
+
+*/
